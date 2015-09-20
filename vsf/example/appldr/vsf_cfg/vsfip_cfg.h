6cec567f3489db8d921fa52302585a38cd882cea
SimonQian <SimonQian@SimonQian.com>
2015-09-20 11:24:03 +0800

    add missing files for appldr

---
+++ vsf/example/appldr/vsf_cfg/vsfip_cfg.h
@@ -0,0 +1,16 @@
+#define VSFIP_CFG_ARPCACHE_SIZE				8
+#define VSFIP_CFG_ARP_TIMEOUT_MS			1000
+#define VSFIP_CFG_TTL_INPUT					10
+#define VSFIP_CFG_UDP_PORT					40000
+#define VSFIP_CFG_TCP_PORT					40000
+
+#define VSFIP_CFG_HEADLEN					64
+#define VSFIP_CFG_HOSTNAME					"vsfip"
+
+#define VSFIP_SOCKET_NUM					8
+#define VSFIP_TCPPCB_NUM					5
+
+#define VSFIP_CFG_TCP_MSS					1500
+
+#define VSFIP_BUFFER_NUM					8
+#define VSFIP_BUFFER_SIZE					1536
