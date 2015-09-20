71de11e5d089aad12a235addd7c4a938918a487d
SimonQian <SimonQian@SimonQian.com>
2015-09-21 05:15:44 +0800

    use assembler predefined macro for standalone app

--- vsf/interfaces/cpu/cortex-m/startup/iar/startup.s
+++ vsf/interfaces/cpu/cortex-m/startup/iar/startup.s
@@ -1,5 +1,5 @@
-	SECTION .entry:CODE:ROOT(4)
-	EXTERN  __iar_program_start
+	SECTION	entry:CODE:ROOT(4)
+	EXTERN	__iar_program_start
 	DATA
-	DCD     __iar_program_start - 0x08080000
+	DCD		__iar_program_start - VSF_APP_BASE
 	END
