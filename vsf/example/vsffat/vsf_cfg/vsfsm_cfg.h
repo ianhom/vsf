5c47ded77ea427835bc2aba01778ca52b8ae3ad8
SimonQian <SimonQian@SimonQian.com>
2016-02-23 17:03:18 +0800

    add vsffat example

---
+++ vsf/example/vsffat/vsf_cfg/vsfsm_cfg.h
@@ -0,0 +1,7 @@
+#define VSFSM_CFG_SYNC_EN				1
+#define VSFSM_CFG_ACTIVE_EN				0
+#define VSFSM_CFG_SM_EN					0
+#define VSFSM_CFG_SUBSM_EN				0
+#define VSFSM_CFG_HSM_EN				0
+#define VSFSM_CFG_PT_EN					1
+#define VSFSM_CFG_LJMP_EN				0
